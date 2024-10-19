345c7614545c4eac68838ba55e9d17c7e6d04b9f
Mark <m_alex_04@hotmail.com>
2024-10-18 21:36:51 -0500

    cambio secciones

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*-- Barra de Navegación--*/
+
+
+
+/*-- Sección de inicio --*/
+
+
+
+/*-- Sección de Funcionalidades--*/
+
+
+
+/*-- Sección de Opiniones--*/
+
+
+
+/*-- Sección de Equipo--*/
+
+
+
+/*-- Sección de Registro--*/
+
+
+
+/*-- Sección Footer--*/
+
+
+
+/*-- Botón volver al inicio--*/
